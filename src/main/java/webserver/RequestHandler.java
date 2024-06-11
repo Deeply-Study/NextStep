488ccdacda44771f50dad3cc8bddf2579fd74f45
Seulg2027 <seulgi2027@dgu.ac.kr>
2024-06-12 08:51:34 +0900

    (요구사항1) URL 연결하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.StringTokenizer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +23,23 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader bf = new BufferedReader(new InputStreamReader(in));
+            String line = bf.readLine();
+
+            if (line != null) {
+                StringTokenizer st = new StringTokenizer(line);
+
+                while (st.hasMoreTokens()) {
+                    String s = st.nextToken();
+                    if (s.matches(".*.html.*")) {
+                        DataOutputStream dos = new DataOutputStream(out);
+                        byte[] body = Files.readAllBytes(new File("./webapp" + s).toPath());
+                        response200Header(dos, body.length);
+                        responseBody(dos, body);
+                    }
+                }
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
