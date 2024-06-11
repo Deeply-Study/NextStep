de074597714929bc0d45cc5c7cfd5ca6f4212b30
ARProxy <hocci0222@kakao.com>
2024-06-11 23:06:25 +0900

    ⚡️ Assignment 1 to 3
    Assignment 1 to 3

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -34,7 +34,6 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             /**
              * GET / HTTP/1.1
@@ -50,52 +49,117 @@ public class RequestHandler extends Thread {
              * Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng;
              * Cookie:JSESSIONID = 6F ADBCE16B68412F08CD19264E23E66B
              */
+
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader buffer = new BufferedReader(new InputStreamReader(in));
             String readLine;
-            String requestLine = buffer.readLine();
-
-            if (requestLine != null && requestLine.startsWith("GET /index.html")) {
-                String[] methodUrl = requestLine.split(" ");
-                String filePath = methodUrl[1].equals("/index.html") ? "/index.html" : methodUrl[1];
-
-                String indexPath = "/Users/proxy/Next-Step/webapp" + filePath;
-                File file = new File(indexPath);
-                if (file.exists()) {
-                    Path path = file.toPath();
-                    byte[] bytes = Files.readAllBytes(path);
-                    DataOutputStream dos = new DataOutputStream(out);
-                    response200Header(dos, bytes.length);
-                    responseBody(dos, bytes);
-                }
-            } else if(requestLine != null && requestLine.startsWith("GET /user/create")) {
-                String[] methodUrl = requestLine.split(" ");
-                System.out.println(Arrays.toString(methodUrl));
-                //[GET, /user/create?userId=javajigi&password=password&name=JaeSung&email=javajigi%40slipp.net, HTTP/1.1]
-                String[] urlSplit = methodUrl[1].split("\\?");
-                System.out.println(Arrays.toString(urlSplit));
-                //[/user/create, userId=javajigi&password=password&name=JaeSung&email=javajigi%40slipp.net]
-                String[] params = urlSplit[1].split("&");
-                System.out.println(Arrays.toString(params));
-                //[userId=javajigi, password=password, name=JaeSung, email=javajigi%40slipp.net]
-                Map<String, String> param = new HashMap<>() {
-                    {
-                        for(String str : params) {
-                            put(str.split("=")[0], str.split("=")[1]);
-                        }
+            String requestLine = null;
+            Map<String, String> headers = new HashMap<>();
+            while ((readLine = buffer.readLine()) != null && !readLine.isEmpty()) {
+                if (requestLine == null) {
+                    requestLine = readLine;
+                } else {
+                    String[] header = readLine.split(": ");
+                    if (header.length == 2) {
+                        headers.put(header[0], header[1]);
                     }
-                };//{password=password, name=JaeSung, userId=javajigi, email=javajigi%40slipp.net}
-                System.out.println(param);
-            } else if(requestLine != null && requestLine.startsWith("POST /user/create")) {
-                IOUtils ioUtils = new IOUtils();
-
-            } else {
-                DataOutputStream dos = new DataOutputStream(out);
-                byte[] body = "Hello World".getBytes();
-                response200Header(dos, body.length);
-                responseBody(dos, body);
+                }
             }
+
+            System.out.println(headers);
+            if (requestLine != null) {
+                String[] methodUrl = requestLine.split(" ");
+                String method = methodUrl[0];
+                String url = methodUrl[1];
+
+                if (method.equals("GET")) {
+                    if (url.equals("/")) {
+                        DataOutputStream dos = new DataOutputStream(out);
+                        byte[] body = "Hello World".getBytes();
+                        response200Header(dos, body.length);
+                        responseBody(dos, body);
+                    } else if (url.equals("/index.html")) {
+                        handleIndexRequest(out, "/index.html");
+                    } else if (url.startsWith("/user/create")) {
+                        handleUserCreate(url);
+                    }
+                } else if (method.equals("POST") && url.equals("/user/create")) {
+                    IOUtils ioUtils = new IOUtils();
+                }
+            }
+            // BufferedReader buffer = new BufferedReader(new InputStreamReader(in));
+            // String readLine;
+            // while((readLine = buffer.readLine()) != null) {
+            //     if (readLine.startsWith("GET /index.html")) {
+            //         String[] methodUrl = readLine.split(" ");
+            //         String filePath = methodUrl[1].equals("/index.html") ? "/index.html" : methodUrl[1];
+            //
+            //         String indexPath = "/Users/proxy/Next-Step/webapp" + filePath;
+            //         File file = new File(indexPath);
+            //         if (file.exists()) {
+            //             Path path = file.toPath();
+            //             byte[] bytes = Files.readAllBytes(path);
+            //             DataOutputStream dos = new DataOutputStream(out);
+            //             response200Header(dos, bytes.length);
+            //             responseBody(dos, bytes);
+            //         }
+            //     } else if(readLine.startsWith("GET /user/create")) {
+            //         String[] methodUrl = readLine.split(" ");
+            //         System.out.println(Arrays.toString(methodUrl));
+            //         //[GET, /user/create?userId=javajigi&password=password&name=JaeSung&email=javajigi%40slipp.net, HTTP/1.1]
+            //         String[] urlSplit = methodUrl[1].split("\\?");
+            //         System.out.println(Arrays.toString(urlSplit));
+            //         //[/user/create, userId=javajigi&password=password&name=JaeSung&email=javajigi%40slipp.net]
+            //         String[] params = urlSplit[1].split("&");
+            //         System.out.println(Arrays.toString(params));
+            //         //[userId=javajigi, password=password, name=JaeSung, email=javajigi%40slipp.net]
+            //         Map<String, String> param = new HashMap<>() {
+            //             {
+            //                 for(String str : params) {
+            //                     put(str.split("=")[0], str.split("=")[1]);
+            //                 }
+            //             }
+            //         };//{password=password, name=JaeSung, userId=javajigi, email=javajigi%40slipp.net}
+            //         System.out.println(param);
+            //     } else if(readLine.startsWith("POST /user/create")) {
+            //         IOUtils ioUtils = new IOUtils();
+            //
+            //     } else {
+            //         DataOutputStream dos = new DataOutputStream(out);
+            //         byte[] body = "Hello World".getBytes();
+            //         response200Header(dos, body.length);
+            //         responseBody(dos, body);
+            //     }
+            // }
         } catch (IOException e) {
             log.error(e.getMessage());
+        }
+    }
+
+    private void handleIndexRequest(OutputStream out, String filePath) throws IOException {
+        String indexPath = "/Users/proxy/Next-Step/webapp" + filePath;
+        File file = new File(indexPath);
+        if (file.exists()) {
+            Path path = file.toPath();
+            byte[] bytes = Files.readAllBytes(path);
+            DataOutputStream dos = new DataOutputStream(out);
+            response200Header(dos, bytes.length);
+            responseBody(dos, bytes);
+        }
+    }
+
+    private void handleUserCreate(String url) {
+        String[] urlSplit = url.split("\\?");
+        if (urlSplit.length > 1) {
+            String[] params = urlSplit[1].split("&");
+            Map<String, String> param = new HashMap<>();
+            for (String str : params) {
+                String[] keyValue = str.split("=");
+                if (keyValue.length == 2) {
+                    param.put(keyValue[0], keyValue[1]);
+                }
+            }
+            System.out.println(param);
         }
     }
 
