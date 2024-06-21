d31353e689731ad2e51f179eba4aac04c35f8453
benjaminuj <chosj1526@gmail.com>
2024-06-22 03:31:20 +0900

    feat: Implement Requirement 5

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -1,11 +1,17 @@
 package util;
 
 public class Constants {
+    // 경로
     public static final String WEB_ROOT = "./webapp";
     public static final String SIGN_UP_PATH = "/user/create";
-    public static final String CONTENTE_LENGTH = "Content-Length";
+    public static final String SIGN_IN_PATH = "/user/login";
+
+    // 구분자
     public static final String HTTP_HEADER_FIELD_SEPARATOR = ": ";
     public static final String QUERY_STRING_PREFIX = "?";
     public static final String PARAM_SEPARATOR = "&";
     public static final String EQUAL_SYMBOL = "=";
+
+    // 그 외
+    public static final String CONTENTE_LENGTH = "Content-Length";
 }
