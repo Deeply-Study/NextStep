94c90fd94255ddc4b6a05a08ba4fc7af6b26263f
benjaminuj <chosj1526@gmail.com>
2024-06-24 15:00:04 +0900

    feat: Implementation of Requirement 6

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,25 +1,28 @@
 package webserver;
 
 import static util.Constants.CONTENTE_LENGTH;
+import static util.Constants.COOKIE_PAIR_SEPARATOR;
+import static util.Constants.COOKIE_SEGMENT_SEPARATOR;
+import static util.Constants.EQUAL_SYMBOL;
+import static util.Constants.SIGN_IN_PAGE;
 import static util.Constants.SIGN_IN_PATH;
 import static util.Constants.SIGN_UP_PATH;
+import static util.Constants.USER_LIST_PAGE;
+import static util.Constants.USER_LIST_PATH;
 import static util.Constants.WEB_ROOT;
 
 import db.DataBase;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
-import java.nio.file.Files;
-import java.security.Key;
+import java.net.URLDecoder;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import model.User;
 import org.slf4j.Logger;
@@ -56,9 +59,45 @@ public class RequestHandler extends Thread {
             // HTTP Header
             Map<String, String> httpHeader = httpRequestUtils.getHeader(br);
 
-            // file
-            String targetPath = WEB_ROOT + requestTarget;
-            File targetFile = new File(targetPath);
+            // 로그인 여부 확인
+            Boolean logined = false;
+
+            if (httpHeader.containsKey("Cookie")) {
+                String[] cookieSegments = httpHeader.get("Cookie").split(COOKIE_SEGMENT_SEPARATOR);
+                for (String cookieSegment : cookieSegments) {
+                    String[] cookiePairs = cookieSegment.split(COOKIE_PAIR_SEPARATOR);
+
+                    for (String cookiePair : cookiePairs) {
+                        String cookieKey = cookiePair.split(EQUAL_SYMBOL)[0];
+                        String cookieValue = cookiePair.split(EQUAL_SYMBOL)[1];
+
+                        if (cookieKey.equals("logined") && cookieValue.equals("true")) {
+                            logined = true;
+                        }
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            // 요청 확인
+            if (requestMethod.equals("GET") && requestTarget.equals(USER_LIST_PATH)) {
+                if (!logined) {
+                    requestTarget = SIGN_IN_PAGE;
+                    response302Header(dos, requestTarget);
+                }
+
+                if (logined) {
+                    int idx = 3;
+                    Collection<User> users = DataBase.findAll();
+                    for(User user : users) {
+                        sb.append("<tr>\n");
+                        sb.append("<th scope=\"row\">" + idx+ "</th> <td>" + user.getUserId() + "</td> <td>" + user.getName() + "</td> <td>" + user.getEmail() + "</td><td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n");
+                        sb.append("</tr>\n");
+                    }
+
+                    requestTarget = USER_LIST_PAGE;
+                }
+            }
 
             if (requestMethod.equals("POST")) {
                 String httpBody = ioUtils.readData(br, Integer.parseInt(httpHeader.get(CONTENTE_LENGTH)));
@@ -83,8 +122,23 @@ public class RequestHandler extends Thread {
                     }
                 }
             }
+            String targetPath = WEB_ROOT + requestTarget;
 
-            byte[] responseBody = Files.readAllBytes(targetFile.toPath());
+            br = new BufferedReader(new FileReader(targetPath));
+            StringBuilder stringB = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                stringB.append(line).append(System.lineSeparator());
+            }
+
+            br.close();
+            String content = stringB.toString();
+            content = content.replace("%userList%", URLDecoder.decode(sb.toString(), "UTF-8")); // Todo: 출력만이 아닌 server에 값을 제대로 저장하기
+
+            // file로 읽는 방법
+//            File targetFile = new File(targetPath);
+//            byte[] responseBody = Files.readAllBytes(targetFile.toPath());
+            byte[] responseBody = content.getBytes();
 
             response200Header(dos, responseBody.length);
             responseBody(dos, responseBody);
